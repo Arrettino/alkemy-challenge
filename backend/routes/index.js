ec705d8c33b326690bbc9c6682784bc837e3cdef
Arrettino <batukearrettino@outlook.com>
2020-12-25 17:17:32 -0300

    routes change router use path of '/' for '/operations'

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use('/', (req, res, next) => {
+router.use('/operations', (req, res, next) => {
   res.send('rad');
 });
 
