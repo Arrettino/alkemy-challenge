c0e89b1affeeeca6c9696466f5d397e079abf2ef
Arrettino <batukearrettino@outlook.com>
2020-12-25 20:46:48 -0300

    operations Model\ C model of operations

---
+++ backend/components/operations/dataAccess/OperationsModel.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../../../db');
+
+module.exports = sequelize.define('operations', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  concept: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  amount: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  date: {
+    type: Sequelize.DATEONLY,
+    allowNull: false,
+  },
+  type: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+});
