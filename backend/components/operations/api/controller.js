ff10ad991c33aef33597fc9f335de4c93a6c672e
Arrettino <batukearrettino@outlook.com>
2020-12-25 19:06:37 -0300

    operations controller\ C CRUD func

---
+++ backend/components/operations/api/controller.js
@@ -0,0 +1,14 @@
+module.exports = {
+  operationsFindAll(req, res) {
+    res.send('rad');
+  },
+  operationsCreate(req, res) {
+    res.send('rad');
+  },
+  operationsUpdate(req, res) {
+    res.send('rad');
+  },
+  operationsDelete(req, res) {
+    res.send('rad');
+  },
+};
