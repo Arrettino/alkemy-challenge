3bd480fa266c791106565f08f06d3431d217bd2d
Arrettino <batukearrettino@outlook.com>
2020-12-25 18:57:02 -0300

    operations routes\ C rest of routes for CRUD

--- backend/components/operations/api/routes.js
+++ backend/components/operations/api/routes.js
@@ -6,4 +6,16 @@ router.get('/', (req, res) => {
   res.send('rad');
 });
 
+router.post('/', (req, res) => {
+  res.send('rad');
+});
+
+router.put('/:id', (req, res) => {
+  res.send('rad');
+});
+
+router.delete('/:id', (req, res) => {
+  res.send('rad');
+});
+
 module.exports = router;
