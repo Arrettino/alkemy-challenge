3e435497796571341f030c1e02cc24fadf674c1d
Arrettino <batukearrettino@outlook.com>
2020-12-25 19:27:11 -0300

    operations services\ C all CRUD functions

---
+++ backend/components/operations/services/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  operationsFindAll() {
+    return ('rad');
+  },
+  operationsCreate() {
+    return ('rad');
+  },
+  operationsUpdate() {
+    return ('rad');
+  },
+  operationsDelete() {
+    return ('rad');
+  },
+};
