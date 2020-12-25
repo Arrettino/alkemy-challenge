31e807586082ff5950d428987514a71d816ec788
Arrettino <batukearrettino@outlook.com>
2020-12-25 19:29:43 -0300

    operations controller\ connect with services

--- backend/components/operations/api/controller.js
+++ backend/components/operations/api/controller.js
@@ -1,14 +1,20 @@
+const operationsServices = require('../services');
+
 module.exports = {
   operationsFindAll(req, res) {
+    operationsServices.operationsFindAll();
     res.send('rad');
   },
   operationsCreate(req, res) {
+    operationsServices.operationsCreate();
     res.send('rad');
   },
   operationsUpdate(req, res) {
+    operationsServices.operationsUpdate();
     res.send('rad');
   },
   operationsDelete(req, res) {
+    operationsServices.operationsDelete();
     res.send('rad');
   },
 };
