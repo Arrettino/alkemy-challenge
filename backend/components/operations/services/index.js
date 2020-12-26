f5ce676de0a02a7e0faa2f31c5e3fbe686f8ac26
Arrettino <batukearrettino@outlook.com>
2020-12-26 02:52:59 -0300

    operations\ call Repo funcs in Service funcs & pass params

--- backend/components/operations/services/index.js
+++ backend/components/operations/services/index.js
@@ -1,16 +1,21 @@
+const { operations } = require('../api/validationSchemas');
 const operationsRepo = require('../dataAccess');
 
 module.exports = {
   findAllOperations() {
+    operationsRepo.findAllOperations();
     return ('rad');
   },
   createOperations(concept, amount, date, type) {
+    operationsRepo.createOperations(concept, amount, date, type);
     return ('rad');
   },
   updateOperations(id, concept, amount, date, type) {
+    operationsRepo.updateOperations(id, concept, amount, date, type);
     return ('rad');
   },
   deleteOperations(id) {
+    operationsRepo.deleteOperations(id);
     return ('rad');
   },
 };
