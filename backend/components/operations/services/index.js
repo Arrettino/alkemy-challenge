f307feed1350ff209b49f261ec50ad475f412a2a
Arrettino <batukearrettino@outlook.com>
2020-12-26 03:07:40 -0300

    operations Services\ C & return response in all funcs

--- backend/components/operations/services/index.js
+++ backend/components/operations/services/index.js
@@ -3,32 +3,32 @@ const operationsRepo = require('../dataAccess');
 module.exports = {
   async findAllOperations() {
     try {
-      await operationsRepo.findAllOperations();
-      return ('rad');
+      const response = await operationsRepo.findAllOperations();
+      return (response);
     } catch (err) {
       return (err);
     }
   },
   async createOperations(concept, amount, date, type) {
     try {
-      await operationsRepo.createOperations(concept, amount, date, type);
-      return ('rad');
+      const response = await operationsRepo.createOperations(concept, amount, date, type);
+      return (response);
     } catch (err) {
       return (err);
     }
   },
   async updateOperations(id, concept, amount, date, type) {
     try {
-      await operationsRepo.updateOperations(id, concept, amount, date, type);
-      return ('rad');
+      const response = await operationsRepo.updateOperations(id, concept, amount, date, type);
+      return (response);
     } catch (err) {
       return (err);
     }
   },
   async deleteOperations(id) {
     try {
-      await operationsRepo.deleteOperations(id);
-      return ('rad');
+      const response = await operationsRepo.deleteOperations(id);
+      return (response);
     } catch (err) {
       return (err);
     }
