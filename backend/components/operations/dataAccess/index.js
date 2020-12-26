6587c82e5f1505ec716be0c7e9fd98423eec8ee2
Arrettino <batukearrettino@outlook.com>
2020-12-26 03:30:20 -0300

    operations Repo\  C querys for all functions

--- backend/components/operations/dataAccess/index.js
+++ backend/components/operations/dataAccess/index.js
@@ -3,28 +3,42 @@ const OperationsModel = require('./OperationsModel');
 module.exports = {
   async findAllOperations() {
     try {
-      return ('rad');
+      const response = await OperationsModel.findAll({});
+      return (response);
     } catch (err) {
       return (err);
     }
   },
   async createOperations(concept, amount, date, type) {
     try {
-      return ('rad');
+      await OperationsModel.create({
+        concept, amount, date, type,
+      });
+      return ({ message: 'Operation created succesfully' });
     } catch (err) {
       return (err);
     }
   },
   async updateOperations(id, concept, amount, date, type) {
     try {
-      return ('rad');
+      await OperationsModel.update({
+        concept, amount, date, type,
+      }, {
+        where: { id },
+      });
+      return ({ message: 'Operation updated succesfully' });
     } catch (err) {
       return (err);
     }
   },
   async deleteOperations(id) {
     try {
-      return ('rad');
+      await OperationsModel.destroy({
+        where: {
+          id,
+        },
+      });
+      return ({ message: 'Operation deleted succesfully' });
     } catch (err) {
       return (err);
     }
