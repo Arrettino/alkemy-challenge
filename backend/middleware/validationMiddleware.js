75d4006672a4b328d2e75eb8da942cfb5e850a50
Arrettino <batukearrettino@outlook.com>
2020-12-26 02:00:40 -0300

    C midlleware/validationMiddleware.js

---
+++ backend/middleware/validationMiddleware.js
@@ -0,0 +1,11 @@
+const middleware = (schema, property) => (req, res, next) => {
+  const { error } = schema.validate(req[property]);
+  if (!error) {
+    next();
+  } else {
+    const { details } = error;
+    const message = details.map((i) => i.message).join(',');
+    res.status(422).json({ error: message });
+  }
+};
+module.exports = middleware;
