ce5f4fd8e2aebd204cf42e1815dceaff5ae82fa3
Arrettino <batukearrettino@outlook.com>
2020-12-26 02:14:06 -0300

    operations\ rename schemas to validationSchemas

--- backend/components/operations/api/routes.js
+++ backend/components/operations/api/routes.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const operationsController = require('./controller');
 const validationMiddleware = require('../../../middleware/validationMiddleware');
-const schema = require('./schemas');
+const validationSchema = require('./validationSchemas');
 
 const router = express.Router();
 
 router.get('/', operationsController.findAllOperations);
 
-router.post('/', validationMiddleware(schema.operations, 'body'), operationsController.createOperations);
+router.post('/',
+  validationMiddleware(validationSchema.operations, 'body'),
+  operationsController.createOperations);
 
-router.put('/:id', validationMiddleware(schema.operationsId, 'params'), validationMiddleware(schema.operations, 'body'), operationsController.updateOperations);
+router.put('/:id',
+  validationMiddleware(validationSchema.operationsId, 'params'),
+  validationMiddleware(validationSchema.operations, 'body'),
+  operationsController.updateOperations);
 
-router.delete('/:id', validationMiddleware(schema.operationsId, 'params'), operationsController.deleteOperations);
+router.delete('/:id',
+  validationMiddleware(validationSchema.operationsId, 'params'),
+  operationsController.deleteOperations);
 
 module.exports = router;
