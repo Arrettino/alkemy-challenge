0a8951914f56138e60138a7c53cd373a927081e0
Arrettino <batukearrettino@outlook.com>
2020-12-26 01:59:16 -0300

    operations schemas\ C schemas operations & operationsId

---
+++ backend/components/operations/api/schemas.js
@@ -0,0 +1,15 @@
+const Joi = require('joi');
+
+const schemas = {
+  operations: Joi.object().keys({
+    concept: Joi.string().min(3).max(30).required(),
+    amount: Joi.number().integer(),
+    date: Joi.string().length(10).required(),
+    type: Joi.string().min(1).max(20).required(),
+  }),
+  operationsId: Joi.object().keys({
+    id: Joi.number().integer().required(),
+  }),
+};
+
+module.exports = schemas;
