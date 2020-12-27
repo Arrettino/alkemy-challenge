51271b200389d5ae8bda2de224f6eb2588b1f9a1
Arrettino <batukearrettino@outlook.com>
2020-12-27 16:36:30 -0300

    F create Operations container

---
+++ frontend/src/container/operations/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './operations.css';
+
+function Home() {
+  return (
+    <div className='operations_container'>
+      <p>Operations</p>
+    </div>
+  );
+}
+
+export default Home;
