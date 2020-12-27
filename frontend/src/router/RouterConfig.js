e3b8a4cdb0f3e4afd1f08b05cdcb39ba3bb549a6
Arrettino <batukearrettino@outlook.com>
2020-12-27 16:37:18 -0300

    F Router\ C route '/operations' component Operations

--- frontend/src/router/RouterConfig.js
+++ frontend/src/router/RouterConfig.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../container/home';
+import Operations from '../container/operations';
 import NotFound from '../container/notFound';
 
 function RouterConfig() {
   return (
     <Switch>
       <Route exact path='/' component={Home} />
+      <Route exact path='/operations' component={Operations} />
       <Route component={NotFound} />
     </Switch>
   );
