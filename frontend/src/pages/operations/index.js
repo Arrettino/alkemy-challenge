bfaa6dcd3c32d9ad7f2fa1963c574d4887b865d9
Arrettino <batukearrettino@outlook.com>
2020-12-27 17:20:02 -0300

    F rename 'container' folder to 'pages'

--- frontend/src/pages/operations/index.js
+++ frontend/src/pages/operations/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Operation from '../../components/operation';
 import './operations.css';
 
 function Operations() {
   return (
     <div className='operations_container'>
       <p>Operations</p>
+      <Operation />
     </div>
   );
 }
