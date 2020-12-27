99326d448a6a0f2d15b0e7fa889e270589473660
Arrettino <batukearrettino@outlook.com>
2020-12-27 20:34:30 -0300

    F app.js\ import bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import RouterConfig from './router/RouterConfig';
+import 'bootstrap/dist/css/bootstrap.css';
 import './app.css';
 
 function App() {
