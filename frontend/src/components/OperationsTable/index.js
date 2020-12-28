8ff2ede801a6151a5bbff45f880e2413f609d001
Arrettino <batukearrettino@outlook.com>
2020-12-28 02:04:10 -0300

    F comp OperationTable\ C div class table-responsive

--- frontend/src/components/OperationsTable/index.js
+++ frontend/src/components/OperationsTable/index.js
@@ -3,23 +3,25 @@ import TableRow from '../OperationsTableRow';
 
 function ListOperations({ operations }) {
   return (
-    <table className='table table-striped'>
-      <thead className='thead-dark'>
-        <tr>
-          <th scope='col'>ID</th>
-          <th scope='col'>Concepto</th>
-          <th scope='col'>Monto</th>
-          <th scope='col'>Fecha</th>
-          <th scope='col'>Tipo</th>
-          <th scope='col'>Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        {operations.map(({ id, concept, amount, date, type }) => (
-          <TableRow id={id} concept={concept} amount={amount} date={date} type={type} />
-        ))}
-      </tbody>
-    </table>
+    <div className='table-responsive'>
+      <table className='table table-striped'>
+        <thead className='thead-dark'>
+          <tr>
+            <th scope='col'>ID</th>
+            <th scope='col'>Concepto</th>
+            <th scope='col'>Monto</th>
+            <th scope='col'>Fecha</th>
+            <th scope='col'>Tipo</th>
+            <th scope='col'>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {operations.map(({ id, concept, amount, date, type }) => (
+            <TableRow id={id} concept={concept} amount={amount} date={date} type={type} />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
