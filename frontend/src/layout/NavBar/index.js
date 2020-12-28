17adf096eeab1bc154e8a3985b7b6130e50e51f9
Arrettino <batukearrettino@outlook.com>
2020-12-28 03:02:53 -0300

    F NavBar\ replace a to Link

--- frontend/src/layout/NavBar/index.js
+++ frontend/src/layout/NavBar/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/pics/logo.svg';
 
 function NavBar({ operations }) {
   return (
     <nav className='navbar navbar-expand-lg navbar-light bg-light'>
       <div className='container-fluid'>
-        <a className='navbar-brand' href='/#'>
+        <Link className='navbar-brand' to='/'>
           <img src={logo} alt='' height='50' className='d-inline-block align-top' />
-        </a>
+        </Link>
         <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
           <span className='navbar-toggler-icon' />
         </button>
         <div className='navbar-collapse collapse w-100 order-3 dual-collapse2'>
           <ul className='navbar-nav ml-auto'>
             <li className='nav-item'>
-              <a className='nav-link' href='/#'>Home</a>
+              <Link className='nav-link' to='/'>Home</Link>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='/#'>Operaciones</a>
+              <Link className='nav-link' to='/operations'>Operaciones</Link>
             </li>
           </ul>
         </div>
