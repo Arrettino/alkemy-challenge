a61755b374fe1c5a151ad7f960990c653fdbd1ce
Arrettino <batukearrettino@outlook.com>
2020-12-28 02:44:37 -0300

    F Operations\ C create button

--- frontend/src/pages/Operations/index.js
+++ frontend/src/pages/Operations/index.js
@@ -6,6 +6,9 @@ function Operations() {
   const [operations] = useState([]);
   return (
     <div className='mt-5'>
+      <button type='button' className='btn btn-success float-right mb-3 mr-3'>
+        Crear
+      </button>
       <OperationTable operations={operations} />
     </div>
   );
