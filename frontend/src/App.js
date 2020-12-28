e051ff0a4a73c0573cae85fa1361bf367a3af14c
Arrettino <batukearrettino@outlook.com>
2020-12-28 01:59:44 -0300

    F app add Layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import RouterConfig from './router/RouterConfig';
+import Layout from './layout';
 import 'bootstrap/dist/css/bootstrap.css';
 import './app.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <RouterConfig />
+      <Layout>
+        <RouterConfig />
+      </Layout>
     </BrowserRouter>
   );
 }
