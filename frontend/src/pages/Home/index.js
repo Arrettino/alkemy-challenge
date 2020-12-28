284d5792fa2b90cbb3d9256c7d21a29d1eceb639
Arrettino <batukearrettino@outlook.com>
2020-12-28 03:18:37 -0300

    F Home\ add container row and col

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -5,16 +5,21 @@ import './home.css';
 function Home() {
   const [operations] = useState([]);
   return (
-    <div className='home_container'>
-      <div style={{ width: '300px' }}>
-        <div className='card text-white bg-success text-center shadow'>
-          <div className='card-body'>
-            <h2>Balance Total</h2>
-            <h3>$5000</h3>
+    <div className='container mt-5'>
+      <div className='row'>
+        <div className='col-sm' style={{ margin: '20px 100px' }}>
+          <div className='card text-white bg-success text-center shadow'>
+            <div className='card-body'>
+              <h2>Balance Total</h2>
+              <h3>$5000</h3>
+            </div>
           </div>
         </div>
+        <div className='col-sm'>
+          <h2 className='text-center' style={{ margin: '0px 0px 30px 0px' }}>Ultimas 10 operaciones</h2>
+          <OperationsList operations={operations} />
+        </div>
       </div>
-      <OperationsList operations={operations} />
     </div>
   );
 }
