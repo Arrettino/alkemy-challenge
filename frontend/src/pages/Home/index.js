d92a6c62be1c46a51df8c4e924605504729fd0a3
Arrettino <batukearrettino@outlook.com>
2020-12-28 03:06:44 -0300

    F Home\ C cart for total balance

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -5,6 +5,14 @@ function Home() {
   return (
     <div className='home_container'>
       <p>Home</p>
+      <div style={{ width: '300px' }}>
+        <div className='card text-white bg-success text-center shadow'>
+          <div className='card-body'>
+            <h2>Balance Total</h2>
+            <h3>$5000</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
