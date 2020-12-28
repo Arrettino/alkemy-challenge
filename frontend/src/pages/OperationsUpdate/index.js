ec9589699311c438261c0db1dfb4855ac8360fe0
Arrettino <batukearrettino@outlook.com>
2020-12-28 13:19:32 -0300

    F\ C OperationsUpdate & add in route 'operations/update

---
+++ frontend/src/pages/OperationsUpdate/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import OperationsForm from '../../components/OperationsForm';
+
+function OperationsUpdate() {
+  const [operation] = useState({});
+  const handleData = (data) => {
+    console.log(data);
+  };
+  return (
+    <>
+      <div className='w-50  mt-5 mx-auto'>
+        <OperationsForm
+          concept={operation.concept}
+          amount={operation.amount}
+          date={operation.date}
+          type={operation.type}
+          handleData={handleData}
+        />
+      </div>
+    </>
+  );
+}
+
+export default OperationsUpdate;
--- frontend/src/router/RouterConfig.js
+++ frontend/src/router/RouterConfig.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Operations from '../pages/Operations';
 import OperationsCreate from '../pages/OperationsCreate';
+import OperationsUpdate from '../pages/OperationsUpdate';
 import NotFound from '../pages/NotFound';
 
 function RouterConfig() {
@@ -11,6 +12,7 @@ function RouterConfig() {
       <Route exact path='/' component={Home} />
       <Route exact path='/operations' component={Operations} />
       <Route exact path='/operations/create' component={OperationsCreate} />
+      <Route exact path='/operations/update' component={OperationsUpdate} />
       <Route component={NotFound} />
     </Switch>
   );
