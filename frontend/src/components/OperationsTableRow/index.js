f893e4fac3386bd9c4b6423b697d63da7fa868c3
Arrettino <batukearrettino@outlook.com>
2020-12-28 14:44:48 -0300

    F OperationsTableRow\ add Link in img edit to /operations/update

--- frontend/src/components/OperationsTableRow/index.js
+++ frontend/src/components/OperationsTableRow/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import edit from '../../assets/pics/edit.svg';
 import remove from '../../assets/pics/delete.svg';
 import './operationsTableRow.css';
@@ -14,7 +15,9 @@ function OperationsTableRow({ id, concept, amount, date, type, actions }) {
       {actions ?
         (
           <td>
-            <img src={edit} alt='edit' className='mr-3 table-cursor-pointer' width='20' />
+            <Link to='/operations/update'>
+              <img src={edit} alt='edit' className='mr-3 table-cursor-pointer' width='20' />
+            </Link>
             <img src={remove} alt='edit' className='ml-3 table-cursor-pointer' width='20' />
           </td>
 
