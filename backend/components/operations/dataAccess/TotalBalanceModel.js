5b22ede457495a4c17e17db99b03b3a361d3b4b9
Arrettino <batukearrettino@outlook.com>
2020-12-28 22:47:29 -0300

    B operations TotalBalanceModel\ U user_id to camel case

--- backend/components/operations/dataAccess/TotalBalanceModel.js
+++ backend/components/operations/dataAccess/TotalBalanceModel.js
@@ -12,7 +12,7 @@ module.exports = sequelize.define('totalBalanceOperations', {
     type: Sequelize.INTEGER,
     allowNull: false,
   },
-  user_id: {
+  userId: {
     type: Sequelize.INTEGER,
     allowNull: false,
   },
