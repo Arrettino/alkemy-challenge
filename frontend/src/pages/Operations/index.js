69425bfc46455cf109a285444531806efcf19547
Arrettino <batukearrettino@outlook.com>
2020-12-29 17:01:34 -0300

    F Operations\ handleErros in handleDelete func

--- frontend/src/pages/Operations/index.js
+++ frontend/src/pages/Operations/index.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import OperationTable from '../../components/OperationsTable';
+import Error from '../../components/Error';
 import { baseUrl } from '../../config';
 import './operations.css';
 
 function Operations() {
   const [operations, setOperations] = useState([]);
+  const [error, setError] = useState(false);
 
   const getOperations = async () => {
     const response = await fetch(`${baseUrl}/operations`);
@@ -17,28 +19,40 @@ function Operations() {
   useEffect(() => {
     getOperations();
   }, []);
-  const handleDelete = async (Id) => {
-    const result = await Swal.fire({
-      title: '¿Estas seguro?',
-      text: 'No se va a poder revertir esta acción',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Si, Eliminar',
-    });
-    if (result.isConfirmed) {
-      await fetch(`${baseUrl}/operations/${Id}`, {
-        method: 'DELETE',
-      });
-      Swal.fire(
-        'Eliminado!',
-        'La operación ha sido eliminada.',
-        'success',
-      );
-    }
 
+  const handleDelete = async (Id) => {
+    try {
+      const result = await Swal.fire({
+        title: '¿Estas seguro?',
+        text: 'No se va a poder revertir esta acción',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, Eliminar',
+      });
+      if (result.isConfirmed) {
+        const response = await fetch(`${baseUrl}/operations/${Id}`, {
+          method: 'DELETE',
+        });
+        if (response.status === 200) {
+          await getOperations();
+          return (Swal.fire(
+            'Eliminado!',
+            'La operación ha sido eliminada.',
+            'success',
+          ));
+        }
+        return (setError(true));
+      }
+    } catch (err) {
+      setError(true);
+    }
   };
+
+  if (error) {
+    return (<Error />);
+  }
   return (
     <div className='mt-5'>
       <Link to='/operations/create'>
