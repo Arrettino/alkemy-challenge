26b1e52818eafb1312f65e1f0b5e5a01073c05ae
Arrettino <batukearrettino@outlook.com>
2020-12-29 19:34:11 -0300

    F OperationsCreate\ handleError of createOperation func

--- frontend/src/pages/OperationsCreate/index.js
+++ frontend/src/pages/OperationsCreate/index.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OperationsForm from '../../components/OperationsForm';
 import { baseUrl } from '../../config';
+import Error from '../../components/Error';
 
 function OperationsCreate() {
+  const history = useHistory();
+  const [error, setError] = useState(false);
 
   const createOperation = async (operation) => {
-    await fetch(`${baseUrl}/operations`, {
-      method: 'POST',
-      body: JSON.stringify(operation),
-      headers: {
-        'Content-type': 'application/json',
-      },
-    });
+    try {
+      const response = await fetch(`${baseUrl}/operations`, {
+        method: 'POST',
+        body: JSON.stringify(operation),
+        headers: {
+          'Content-type': 'application/json',
+        },
+      });
+      if (response.status === 200) {
+        return (history.push('/operations'));
+      }(
+        setError(true)
+      );
+    } catch (err) {
+      setError(true);
+    }
   };
 
   const handleData = (operation) => {
     console.log(operation);
     createOperation(operation);
   };
+
+  if (error) {
+    return (<Error />);
+  }
+
   return (
     <>
       <div className='w-50  mt-5 mx-auto'>
