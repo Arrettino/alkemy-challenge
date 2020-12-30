404a4767c03c7be830b17734ebb9d470e580c4a0
Arrettino <batukearrettino@outlook.com>
2020-12-29 23:33:20 -0300

    B operations validation\ add required & positive to operation.amount

--- backend/components/operations/api/validationSchemas.js
+++ backend/components/operations/api/validationSchemas.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const schemas = {
   operations: Joi.object().keys({
     concept: Joi.string().min(3).max(30).required(),
-    amount: Joi.number().integer(),
+    amount: Joi.number().integer().positive().required(),
     date: Joi.string().length(10).required(),
     type: Joi.string().min(1).max(20).required(),
   }),
