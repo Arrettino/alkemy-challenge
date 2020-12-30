fcec1f18ac6f1f353bfaae1b756eebee1ce8ee65
Arrettino <batukearrettino@outlook.com>
2020-12-30 10:10:13 -0300

    B app\ add express.statics if nodeEnv !== development

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const config = require('config');
 const cors = require('cors');
+const path = require('path');
 const initDatabase = require('./db/initDatabase');
 
 const router = require('./routes');
@@ -11,14 +12,20 @@ const app = express();
 
 initDatabase();
 
-if (config.get('server.nodeEnv') === 'development') {
-  app.use(cors());
-}
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/server', router);
+
+if (config.get('server.nodeEnv') === 'development') {
+  app.use(cors());
+} else {
+  app.use(express.static(path.join(__dirname, './../frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+  });
+}
 
 module.exports = app;
